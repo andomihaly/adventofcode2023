68c916a801392c29a728c194dfbf1b4af28046fe
andomihaly <ando.mihaly@cleanit.hu>
2023-12-17 18:40:44 +0100

    add unit test support with sub folder structure

---
+++ day01/test/numberfinder/findnumberintexttest.py
@@ -0,0 +1,8 @@
+import unittest
+
+class FindNumberInTextTest(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
---
+++ day01/test/rununittest.py
@@ -0,0 +1,5 @@
+import unittest
+from numberfinder.findnumberintexttest import FindNumberInTextTest
+
+if __name__ == '__main__':
+    unittest.main()
