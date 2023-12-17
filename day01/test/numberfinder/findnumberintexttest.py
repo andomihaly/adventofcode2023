bbeea7492f01958a7ce3b348d7ac156320857a03
andomihaly <ando.mihaly@cleanit.hu>
2023-12-17 19:01:26 +0100

    add unit test which is call the app class

--- day01/test/numberfinder/findnumberintexttest.py
+++ day01/test/numberfinder/findnumberintexttest.py
@@ -1,8 +1,12 @@
 import unittest
+from findnumberintext import FindNumberInText
+
 
 class FindNumberInTextTest(unittest.TestCase):
 
+    def setUp(self):
+        self.findNumber = FindNumberInText()
     def test_something(self):
-        self.assertEqual(True, True)
+        self.assertEqual(12, self.findNumber.findNumber("12"))
 
 
--- day01/test/rununittest.py
+++ day01/test/rununittest.py
@@ -1,4 +1,11 @@
 import unittest
+import sys
+import os
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir=parent_dir.replace("\\test", "")
+sys.path.append(root_dir+"\\src\\numberfinder")
+sys.path.append(root_dir+"\\src\\logger")
+
 from numberfinder.findnumberintexttest import FindNumberInTextTest
 
 if __name__ == '__main__':
