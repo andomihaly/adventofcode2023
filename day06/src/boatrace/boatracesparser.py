1de3dba54e1fa96194517316e6d6e1af22fa30de
andomihaly <ando.mihaly@cleanit.hu>
2023-12-25 21:41:03 +0100

    parse input rases

--- day06/src/boatrace/boatracesparser.py
+++ day06/src/boatrace/boatracesparser.py
@@ -1,5 +1,14 @@
+import re
 from loggercontext import LoggerContext
 
 class BoatRacesParser():
     logger = LoggerContext()
-    pass
+    def parse(self, input):
+        times=re.findall(r'\d+',input[0])
+        self.logger.debug(str(times))
+        distances=re.findall(r'\d+',input[1])
+        self.logger.debug(str(distances))
+        races=[]
+        for i in range(0,len(times)):
+            races.append([int(times[i]),int(distances[i])])
+        return races
--- day06/test/boatrace/boatracesparsertest.py
+++ day06/test/boatrace/boatracesparsertest.py
@@ -2,5 +2,16 @@ import unittest
 from boatracesparser import BoatRacesParser
 
 class BoatRacesParserTest(unittest.TestCase):
-    def setUp(self):
-        pass
+    def test_parseRace(self):
+        input="Time:      1234\nDistance:  54321"
+        bp=BoatRacesParser()
+        result=bp.parse(input.splitlines())
+        self.assertEqual(1, len(result))
+        self.assertEqual([1234,54321], result[0])
+
+    def test_parseRaces(self):
+        input="Time:      1  22   33\nDistance:  5  66  777"
+        bp=BoatRacesParser()
+        result=bp.parse(input.splitlines())
+        self.assertEqual(3, len(result))
+        self.assertEqual([[1,5],[22,66],[33,777]], result)
