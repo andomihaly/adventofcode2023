4a3342b89bac414dda18b8a27cb896885095d33a
andomihaly <ando.mihaly@cleanit.hu>
2023-12-27 08:11:52 +0100

    add running time feature

--- dayinit/appstarter.py
+++ dayinit/appstarter.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 parent_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(parent_dir)
@@ -11,10 +12,15 @@ from textfileloader import TextFileLoader
 from interactor import Interactor
 
 if __name__ == '__main__':
+    start_time = time.time()
+
     folderOfFiles = "src\\loader\\"
     fileName = "example.txt"
     # fileName = "puzzleinput.txt"
-
     tfl = TextFileLoader(folderOfFiles + fileName)
     interactor = Interactor(tfl)
     interactor.run()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("Elapsed time: ", elapsed_time)
